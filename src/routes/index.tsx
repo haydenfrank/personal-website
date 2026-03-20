dd5575031fae06ac4a50ca2dcd3014922c26e94a
Hayden Frank <haydog2006@gmail.com>
2026-03-20 00:41:16 -0400

    made links work

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,13 +7,17 @@ function App() {
     <h1>Welcome to my website!</h1>
     <ul>
       <li>
-        <a href="media.haydenbfrank.com">
+        <a href="https://media.haydenbfrank.com">
           Access my Emby server.
         </a>
-        <a href="nas.haydenbfrank.com">
+      </li>
+      <li>
+        <a href="https://nas.haydenbfrank.com">
           Access my nas through DSM.
         </a>
-        <a href="requests.haydenbfrank.com">
+      </li>
+      <li>
+        <a href="https://requests.haydenbfrank.com">
           Access my Jellyseerr requesting service.
         </a>
       </li>
