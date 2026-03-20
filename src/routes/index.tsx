3a9b7089743a5b3a35db327afbdd864e22044daa
Hayden Frank <haydog2006@gmail.com>
2026-03-20 00:36:05 -0400

    put links on my website

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,19 @@ export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (<>
-    <h1>Hi parker!</h1>
+    <h1>Welcome to my website!</h1>
+    <ul>
+      <li>
+        <a href="media.haydenbfrank.com">
+          Access my Emby server.
+        </a>
+        <a href="nas.haydenbfrank.com">
+          Access my nas through DSM.
+        </a>
+        <a href="requests.haydenbfrank.com">
+          Access my Jellyseerr requesting service.
+        </a>
+      </li>
+    </ul>
   </>)
 }
